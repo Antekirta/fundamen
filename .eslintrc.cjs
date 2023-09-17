fb7e496544dd743dd671ce18deb92774c227676a
Antekirta <murashkin.kirill.konst@gmail.com>
2023-09-16 21:12:34 -0300

    FUN-2 FE / Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended'
+  ],
+  "rules": {
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 1
+      },
+      "multiline": {
+        "max": 1
+      }
+    }]
+  }
+};
