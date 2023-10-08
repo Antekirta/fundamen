5439dc8bb1acbc715ee13a3b003946077145c6fe
Antekirta <murashkin.kirill.konst@gmail.com>
2023-10-08 08:45:05 -0300

    FUN-1 FE / TheUnput.stories.js / Init

---
+++ stories/TheInput.stories.js
@@ -0,0 +1,13 @@
+import TheInput from '../components/TheInput.vue'
+
+export default {
+  title: 'Example/Input',
+  component: TheInput,
+  tags: ['autodocs']
+}
+
+export const Primary = {
+  args: {
+    label: 'Name'
+  }
+}
