6471dd0e5dee35b92fd237780e8235b171888a7b
Antekirta <murashkin.kirill.konst@gmail.com>
2023-10-21 18:23:08 -0300

    FUN-1 FE / TheInput.stories.js / Add decorator

--- stories/TheInput.stories.js
+++ stories/TheInput.stories.js
@@ -3,7 +3,15 @@ import TheInput from '../components/TheInput.vue'
 export default {
   title: 'Example/Input',
   component: TheInput,
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  args: {
+    label: 'Name'
+  },
+  decorators: [
+    () => ({
+      template: '<div style="width: 280px;"><story /></div>'
+    })
+  ]
 }
 
 export const Primary = {
