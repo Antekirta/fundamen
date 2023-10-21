b7e1d60091e9ef63a0f13ffd56a4fc74fe48607b
Antekirta <murashkin.kirill.konst@gmail.com>
2023-10-21 18:14:30 -0300

    FUN-1 FE / tailwind.config.cjs / Change the extension and import type in order to import it rfeely across the app

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
+// DEFAULT THEME: https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// module.exports = {
+export default {
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
