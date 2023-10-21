a2410bf3de89a1d3b04a21a96477ed828159ea95
Antekirta <murashkin.kirill.konst@gmail.com>
2023-10-21 18:23:38 -0300

    FUN-1 FE / TheInput.stories.js / Create basic stories

--- stories/TheInput.stories.js
+++ stories/TheInput.stories.js
@@ -16,6 +16,78 @@ export default {
 
 export const Primary = {
   args: {
-    label: 'Name'
+    helpText: 'Beasts were standing by the door.',
+    cornerHint: 'Optional'
+  }
+}
+
+export const Inline = {
+  args: {
+    inline: true,
+    helpText: 'Beasts were standing by the door.'
+  }
+}
+
+export const Required = {
+  args: {
+    required: true
+  }
+}
+
+export const WithErrors = {
+  args: {
+    errors: [
+      'They were shot.',
+      'And they died.'
+    ]
+  }
+}
+
+export const LabelOverlapping = {
+  args: {
+    labelType: 'overlapping'
+  }
+}
+
+export const LabelInset = {
+  args: {
+    labelType: 'inset'
+  }
+}
+
+export const WithAddon = {
+  args: {
+    addon: 'http://'
+  }
+}
+
+export const PrependIcon = {
+  args: {
+    prependIcon: 'envelope'
+  }
+}
+
+export const AppendIcon = {
+  args: {
+    appendIcon: 'envelope'
+  }
+}
+
+export const BorderBottom = {
+  args: {
+    borderType: 'bottom'
+  }
+}
+
+export const Pill = {
+  args: {
+    pill: true
+  }
+}
+
+export const StyledFromOutside = {
+  args: {
+    borderType: 'bottom',
+    inputExtraClasses: 'bg-slate-200'
   }
 }
