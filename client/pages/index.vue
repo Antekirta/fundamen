cf4398657a2c053fc7fdd8d10e273ab73cee8d32
Antekirta <murashkin.kirill.konst@gmail.com>
2023-12-28 21:11:21 -0300

    FUN-6 Prepare SSR

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <h1>Main page</h1>
-</template>
-<script>
+  <div>
+    <h1>Main page</h1>
 
-export default {
-}
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+/* global useFetch */
+const { data } = await useFetch('http://localhost:3002/test')
 </script>
