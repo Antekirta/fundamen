1c3565df296cf628e15a71d6aca8b0a8266ed3e4
Antekirta <murashkin.kirill.konst@gmail.com>
2024-02-11 10:59:49 -0300

    FUN-14 Use TheHeader.vue and TheDescription.vue

--- client/components/Organisms/MainPage/TheHeroSection.vue
+++ client/components/Organisms/MainPage/TheHeroSection.vue
@@ -49,12 +49,14 @@
       <div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
         <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
           <div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <the-header as="h1">
               We’re changing the way people connect.
-            </h1>
-            <p class="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+            </the-header>
+
+            <the-description>
               Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis cupidatat mollit aute velit. Et labore commodo nulla aliqua proident mollit ullamco exercitation tempor. Sint aliqua anim nulla sunt mollit id pariatur in voluptate cillum.
-            </p>
+            </the-description>
+
             <div class="mt-10 flex items-center gap-x-6">
               <the-button color="indigo">
                 Get started
@@ -65,6 +67,7 @@
               </the-button>
             </div>
           </div>
+
           <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
             <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
               <div class="relative">
@@ -76,6 +79,7 @@
                 <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
               </div>
             </div>
+
             <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
               <div class="relative">
                 <img
@@ -94,6 +98,7 @@
                 <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
               </div>
             </div>
+
             <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
               <div class="relative">
                 <img
@@ -120,4 +125,6 @@
 </template>
 <script setup lang="ts">
 import TheButton from '@/components/Atoms/TheButton.vue'
+import TheHeader from '@/components/Atoms/TheHeader.vue'
+import TheDescription from '@/components/Atoms/TheDescription.vue'
 </script>
