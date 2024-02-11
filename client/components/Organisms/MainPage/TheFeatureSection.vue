356b3d1ee3f33e7c6ca753a71333efe72023c802
Antekirta <murashkin.kirill.konst@gmail.com>
2024-02-11 11:36:57 -0300

    FUN-14 Create and use ThePreheader.vue and TheItemWithIcon.vue components

--- client/components/Organisms/MainPage/TheFeatureSection.vue
+++ client/components/Organisms/MainPage/TheFeatureSection.vue
@@ -2,37 +2,31 @@
   <div class="py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:text-center">
-        <h2 class="text-base font-semibold leading-7 text-indigo-600">
+        <the-preheader>
           Deploy faster
-        </h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        </the-preheader>
+
+        <the-header
+          class="mt-2"
+          as="h2"
+        >
           Everything you need to deploy your app
-        </p>
-        <p class="mt-6 text-lg leading-8 text-gray-600">
+        </the-header>
+
+        <the-description>
           Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.
-        </p>
+        </the-description>
       </div>
+
       <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
         <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-          <div
+          <the-item-with-icon
             v-for="feature in features"
             :key="feature.name"
-            class="relative pl-16"
-          >
-            <dt class="text-base font-semibold leading-7 text-gray-900">
-              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                <component
-                  :is="feature.icon"
-                  class="h-6 w-6 text-white"
-                  aria-hidden="true"
-                />
-              </div>
-              {{ feature.name }}
-            </dt>
-            <dd class="mt-2 text-base leading-7 text-gray-600">
-              {{ feature.description }}
-            </dd>
-          </div>
+            :title="feature.name"
+            :description="feature.description"
+            :icon="feature.icon"
+          />
         </dl>
       </div>
     </div>
@@ -41,6 +35,10 @@
 
 <script setup>
 import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/vue/24/outline'
+import ThePreheader from '@/components/Atoms/ThePreheader.vue'
+import TheHeader from '@/components/Atoms/TheHeader.vue'
+import TheDescription from '@/components/Atoms/TheDescription.vue'
+import TheItemWithIcon from '@/components/Atoms/TheItemWithIcon.vue'
 
 const features = [
   {
