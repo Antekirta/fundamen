f33d636cf35188576df6e7147494421cf74ed89e
Antekirta <murashkin.kirill.konst@gmail.com>
2024-02-21 18:56:40 -0300

    FUN-46 Create TheLoginModal component

--- client/components/Molecules/common/buttons/TheLoginButton.vue
+++ client/components/Molecules/common/buttons/TheLoginButton.vue
@@ -1,6 +1,32 @@
 <template>
-  <a
-    href="#"
-    class="text-sm font-semibold leading-6 text-gray-900"
-  >Log in <span aria-hidden="true">&rarr;</span></a>
+  <div>
+    <a
+      href="#"
+      class="text-sm font-semibold leading-6 text-gray-900"
+      @click.prevent="openLoginModal"
+    >Log in <span aria-hidden="true">&rarr;</span></a>
+
+    <the-login-modal
+      :is-open="modals.login.isOpen"
+      @close="closeLoginModal"
+    />
+  </div>
 </template>
+
+<script lang="ts" setup>
+import TheLoginModal from '@/components/Organisms/modals/TheLoginModal.vue'
+
+const modals = reactive({
+  login: {
+    isOpen: false
+  }
+})
+
+const openLoginModal = () => {
+  modals.login.isOpen = true
+}
+
+const closeLoginModal = () => {
+  modals.login.isOpen = false
+}
+</script>
