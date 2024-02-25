7561465b4ec90374184ffa00061d842115c7a4c2
Antekirta <murashkin.kirill.konst@gmail.com>
2024-02-24 22:35:56 -0300

    FUN-47 TheSubItem.vue / Refactor to use @apply

--- client/components/Molecules/common/TheMainMenu/TheSubItem.vue
+++ client/components/Molecules/common/TheMainMenu/TheSubItem.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="group relative flex gap-x-6 rounded-lg p-4 leading-6 text-lg hover:bg-gray-50">
+  <div class="the-sub-item">
     <div
       v-if="item.icon"
-      class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white"
+      class="the-sub-item__icon-container"
     >
       <component
         :is="item.icon"
-        class="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+        class="the-sub-item__icon"
         aria-hidden="true"
       />
     </div>
-    <div class="flex-auto">
+
+    <div class="the-sub-item__container">
       <a
         :href="item.href"
-        class="block text-lg font-semibold text-gray-900"
+        class="the-sub-item__title"
       >
         {{ item.name }}
         <span class="absolute inset-0" />
       </a>
+
       <p
         v-if="item.description"
-        class="mt-1 text-gray-600"
+        class="the-sub-item__description"
       >
         {{ item.description }}
       </p>
@@ -35,3 +37,29 @@ defineProps<{
   item: ListItemInterface
 }>()
 </script>
+
+<style lang="scss">
+.the-sub-item {
+  @apply relative flex gap-x-6 rounded-lg p-4 leading-6 text-lg hover:bg-gray-50;
+
+  &__icon-container {
+    @apply mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white;
+  }
+
+  &__icon {
+    @apply h-6 w-6 text-gray-600 hover:text-indigo-600;
+  }
+
+  &__container {
+    @apply flex-1;
+  }
+
+  &__title {
+    @apply block text-lg font-semibold text-gray-900;
+  }
+
+  &___description {
+    @apply mt-1 text-gray-600;
+  }
+}
+</style>
