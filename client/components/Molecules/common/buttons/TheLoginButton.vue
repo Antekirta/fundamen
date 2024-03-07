c985de08c1a597117ed81091d9240c3d9c952c50
Antekirta <murashkin.kirill.konst@gmail.com>
2024-03-07 19:26:41 -0300

    FUN-47 TheLoginButton.vue / Style TheButton in a standard way

--- client/components/Molecules/common/buttons/TheLoginButton.vue
+++ client/components/Molecules/common/buttons/TheLoginButton.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="the-login-button">
     <the-button
-      class="the-login-button__btn"
-      color="pink"
+      color="light"
       @click.prevent="openLoginModal"
     >
       <span class="uppercase">sign in</span>
@@ -36,11 +35,5 @@ const closeLoginModal = () => {
 
 <style lang="scss">
 .the-login-button {
-
-  &__btn {
-    @apply bg-pink;
-    @apply text-xl tracking-wider font-semibold leading-6 text-brown hover:ring-4 hover:ring-red hover:ring-offset-2 hover:ring-offset-brown;
-    @apply transition duration-200;
-  }
 }
 </style>
