5bd8cfa97a25ee005a3a81687491e2838e50138d
Antekirta <murashkin.kirill.konst@gmail.com>
2024-10-31 21:59:07 -0300

    master Fix routing for development

--- client/plugins/custom-fetch.js
+++ client/plugins/custom-fetch.js
@@ -1,7 +1,15 @@
 export default defineNuxtPlugin({
   setup () {
+    const getBaseUrl = () => {
+      if (process.env.NODE_ENV === 'development') {
+        return process.client ? '/api' : 'http://localhost:3002'
+      }
+
+      return process.client ? '/api' : 'http://nest:3002'
+    }
+
     const customFetch = $fetch.create({
-      baseURL: process.client ? '/api' : 'http://nest:3002'
+      baseURL: getBaseUrl()
     })
 
     return {
